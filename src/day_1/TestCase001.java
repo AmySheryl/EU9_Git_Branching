661a9ffd3d088395c891f2182efd1181e7d863cd
Amy Sheryl <amysheryl00@gmail.com>
2022-09-08 11:25:59 +0300

    created the test case 001

---
+++ src/day_1/TestCase001.java
@@ -0,0 +1,12 @@
+package day_1;
+
+public class TestCase001 {
+
+    public static void main(String[] args) {
+
+        //test case 001
+        // Testing login
+
+        //Using valid credentials
+    }
+}
